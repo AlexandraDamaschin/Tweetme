80d2a5e2fc1fe04dc555de2f8c74afe2332e248d
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-07-15 19:04:14 +0100

    migrations

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -3,6 +3,4 @@ from django.db import models
 
 # Create your models here.
 class Tweet(models.Model):
-    content1 = models.TextField()
-    content2 = models.TextField()
-    content3 = models.TextField()
+    content = models.TextField()
