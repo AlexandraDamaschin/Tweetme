cc0fc29769f6f2a9d750044b19039df9771ff007
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-07-15 19:02:17 +0100

    tweets to add in /admin

---
+++ src/tweets/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class Tweet(models.Model):
+    content1 = models.TextField()
+    content2 = models.TextField()
+    content3 = models.TextField()
