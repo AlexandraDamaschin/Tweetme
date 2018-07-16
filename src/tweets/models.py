90a14269cb3319a0335380f26551a735623ea141
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-07-16 21:21:18 +0100

    user associated to tweet

--- src/tweets/models.py
+++ src/tweets/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 
 # Create your models here.
 class Tweet(models.Model):
-    # user to be associated
+    # user to be associated to tweet
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.CharField(max_length=140)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
