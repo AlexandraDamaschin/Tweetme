971f06a9c01a1ad54dc93bdab4676574b63dbffd
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-07-17 19:15:22 +0100

    get from database list and by id

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 
+from .models import Tweet
+
 
 # Create your views here.
 
 # Retrieve information from database
 def tweet_detail_view(request, id=1):
+    obj = Tweet.objects.get(id=id)  # GET from database
+    print(obj)
+    context = {
+        "object": obj
+    }
     return render(request, "tweets/detail_view.html", {})
 
 
 def tweet_list_view(request):
+    queryset = Tweet.objects.all()
+    print(queryset)
+    context = {
+        "object_list": queryset
+    }
     return render(request, "tweets/list_view.html", {})
