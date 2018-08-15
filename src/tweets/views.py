732616a81c8c1db722cf29156dfdcd65d4a20de6
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-08-15 19:28:06 +0100

    class based view

--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 
 from .models import Tweet
 
 
 # Create your views here.
+# UPDATE
+
+# DELETE
+
+# LIST/SEARCH
+
+class TweetDetailView(DetailView):
+    template_name = "tweets/detail_view.html"
+    querySet = Tweet.objects.all()
+
+    def get_object(self):
+        return Tweet.objects.get(id=1)
+
+
+class TweetListView(ListView):
+    template_name = "tweets/list_view.html"
+    querySet = Tweet.objects.all()
+
 
 # Retrieve information from database
 def tweet_detail_view(request, id=1):
