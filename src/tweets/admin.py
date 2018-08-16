61e5ce79323f827ec0b932597948abbaea838023
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-08-16 20:29:19 +0100

    create view

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -8,7 +8,9 @@ from .models import Tweet
 
 
 class TweetModelAdmin(admin.ModelAdmin):
-    form = TweetModelForm
+    # form = TweetModelForm
+    class Meta:
+        model = Tweet
 
 
 admin.site.register(Tweet, TweetModelAdmin)
--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 
 from .views import tweet_detail_view, tweet_list_view
 from .views import TweetListView, TweetDetailView
+from .views import (TweetDetailView, TweetListView, TweetCreateView)
 
 urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='list'),
-    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail')
+    url(r'^create/$', TweetCreateView.as_view(), name='create'),  # /tweet/create/
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail')  # /tweet/1/
 ]
