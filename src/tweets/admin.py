6ccbcb89b31ecfa74f23012bd275acfae97411f4
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-08-16 19:49:44 +0100

    Model Form

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 
-from .models import Tweet
 from .forms import TweetModelForm
+from .models import Tweet
 
-# Register your models here.
 
-admin.site.register(Tweet)
+# admin.site.register(Tweet)
 
 
 class TweetModelAdmin(admin.ModelAdmin):
