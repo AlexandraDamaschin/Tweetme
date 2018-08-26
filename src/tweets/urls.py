14590e510ea7f4ee01c195b1840743c22bf7afa1
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-08-26 13:19:23 +0100

    urls

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls import url
 
-from .views import TweetListView
-from .views import (TweetDetailView, TweetCreateView, TweetUpdateView)
+from .views import (
+    TweetDetailView,
+    TweetCreateView,
+    TweetUpdateView,
+    TweetDeleteView,
+    TweetListView
+)
 
 urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='list'),
     url(r'^create/$', TweetCreateView.as_view(), name='create'),  # /tweet/create/
     url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),  # /tweet/1/
     url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'),  # /tweet/1/update
+    url(r'^(?P<pk>\d+)/delete/$', TweetDeleteView.as_view(), name='delete'),  # /tweet/1/delete
+
 ]
